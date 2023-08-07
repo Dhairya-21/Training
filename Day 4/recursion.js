5e2c10483ed9b0481507fd87a2d24246c223049e
Dhairya-21 <141601531+Dhairya-21@users.noreply.github.com>
2023-08-07 10:18:04 +0530

    Add files via upload

---
+++ Day 4/recursion.js
@@ -0,0 +1,9 @@
+// function hasRepeats (str) {
+//     return /(.).*\1/.test(str);
+// }
+// var text = "test".split("");
+// text.some(function(v,i,a){
+//   return a.lastIndexOf(v)!=i;
+// });
+
+// console.log(hasRepeats("Dhairya"));
